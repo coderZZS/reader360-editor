79ae3e812ba89747fb27fb6e714559813ddf9122
毛大爷 <1874145841@qq.com>
2023-04-10 22:26:34 +0800

    feat: 登录页

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -3,6 +3,7 @@ import { lazy } from 'react'
 const Main = lazy(() => import('@/views/main/index.tsx'))
 const Editor = lazy(() => import('@/views/editor/index.tsx'))
 const Home = lazy(() => import('@/views/home/index.tsx'))
+const Login = lazy(() => import('@/views/login/index.tsx'))
 
 export const mainRoutes: Array<RouteObject> = [
     {
@@ -12,6 +13,10 @@ export const mainRoutes: Array<RouteObject> = [
     {
         path: '/editor',
         element: <Editor />,
+    },
+    {
+        path: '/login',
+        element: <Login />,
     },
 ]
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,29 @@ const colors = require('tailwindcss/colors')
 module.exports = {
     content: [],
     theme: {
-        extend: {},
+        extend: {
+            backgroundImage: {
+                'login-bg': "url('/public/image/loginBG.png')",
+            },
+        },
         colors: {
             abs: 'pink',
+            fontAbs: '#1a1a1a',
+            pur: 'purple',
+        },
+        boxShadow: {
+            sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
+            DEFAULT: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
+            md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+            lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+            xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
+            '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
+            '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+            inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
+            none: 'none',
+            myy: '3px 0px 9px 2px #0000007a',
         },
     },
     plugins: [],
+    dark: 'class',
 }
